bc0246afe724f875ec51ee7ae8954c15137e8943
Cesar Landeros Delgado <chichozell@gmail.com>
2015-03-10 21:11:45 -0600

    ejercicio de la segunda sesion del curso

--- app/modules/recipes/recipes.config.js
+++ app/modules/recipes/recipes.config.js
@@ -4,8 +4,5 @@
 angular.module('Reciper.recipes', ['ngRoute', 'ngResource'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/recipes', {
-    templateUrl: 'modules/recipes/recipes_list.html',
-    controller: 'RecipesController'
-  });
-}])
+  
+}]);
--- app/modules/recipes/recipes.controller.js
+++ app/modules/recipes/recipes.controller.js
@@ -4,8 +4,51 @@
 angular.module('Reciper.recipes')
     .controller('RecipesController', RecipesController);
 
-RecipesController.$inject = ['$scope', 'RecipesService'];
+RecipesController.$inject = ['RecipesService'];
 
-function RecipesController($scope, RecipesService) {
-    $scope.recipes = RecipesService.query();
+function RecipesController(RecipesService) {
+    this.recipes = RecipesService.query();
+
+    this.guardar = guardar;
+    this.tipos   = [];
+    this.titulo  = 'hola';
+
+    this.tipos = [{
+        id: 'd',
+        nombre: 'Desayuno'
+    },{
+        id: 'c',
+        nombre: 'Comida'
+    },{
+        id: 'ce',
+        nombre: 'Cena'
+    }]
+
+    function guardar() {
+        this.recipes.unshift({
+            name: this.nombre,
+            category: this.tipo,
+            description: this.descripcion
+        })
+    }
+}
+//--------------------------------------------------
+angular.module('Reciper.recipes')
+    .controller('formularioController', formularioController);
+
+formularioController.$inject = [];
+
+function formularioController() {
+
+    
+    
+}
+//----------------------------------------------------
+angular.module('Reciper.recipes')
+    .controller('listaController', listaController);
+
+listaController.$inject = [];
+
+function listaController() {
+
 }
