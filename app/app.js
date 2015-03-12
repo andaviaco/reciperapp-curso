a79c19d53500e077d485d5d397264f057f96edd8
Andrés <andpy.me@gmail.com>
2015-03-12 14:05:21 -0600

    Add recipe with simple form

--- app/app.js
+++ app/app.js
@@ -2,10 +2,10 @@
 
 // Declare app level module which depends on views, and components
 angular.module('Reciper', [
-  'ngRoute',
-  'Reciper.recipes'
+    'ngRoute',
+    'Reciper.recipes'
 ])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/recipes'});
+    $routeProvider.otherwise({redirectTo: '/recipes'});
 }]);
--- app/modules/recipes/recipes.controller.js
+++ app/modules/recipes/recipes.controller.js
@@ -7,5 +7,61 @@ angular.module('Reciper.recipes')
 RecipesController.$inject = ['$scope', 'RecipesService'];
 
 function RecipesController($scope, RecipesService) {
-    $scope.recipes = RecipesService.query();
+    $scope.recipes    = RecipesService.query();
+    $scope.categories = [
+        {
+            key: "0",
+            value: "Desayuno"
+        },
+        {
+            key: "1",
+            value: "Comida"
+        },
+        {
+            key: "2",
+            value: "Cena"
+        },
+        {
+            key: "3",
+            value: "Otra"
+        }
+    ];
+
+    $scope.create        = create;
+    $scope.addIngredient = addIngredient;
+    $scope.addStep       = addStep;
+
+    resetFormValues();
+
+    function resetFormValues() {
+        $scope.new_recipe             = {};
+        $scope.new_recipe.ingredients = [{}];
+        $scope.new_recipe.steps       = [{}];
+        $scope.new_recipe.comments    = [];
+    }
+
+    function create(recipe) {
+        console.debug(recipe);
+
+        if (angular.isUndefined(
+                recipe.ingredients[recipe.ingredients.length-1].name)) {
+            recipe.ingredients.pop();
+        }
+
+        if (angular.isUndefined(
+                recipe.steps[recipe.steps.length-1].description)) {
+            recipe.steps.pop();
+        }
+
+        $scope.recipes.unshift(recipe);
+        resetFormValues();
+    }
+
+    function addIngredient() {
+        $scope.new_recipe.ingredients.push({});
+    }
+
+    function addStep() {
+        $scope.new_recipe.steps.push({});
+    }
 }
