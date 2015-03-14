801cbf285d2c684a9a7b960b3416a57fea1952f8
Cesar Landeros Delgado <chichozell@gmail.com>
2015-03-13 19:39:26 -0600

    actividad de la sesion 3

--- app/modules/recipes/recipes.controller.js
+++ app/modules/recipes/recipes.controller.js
@@ -1,11 +1,33 @@
 
-'use strict';
-
 angular.module('Reciper.recipes')
     .controller('RecipesController', RecipesController);
 
-RecipesController.$inject = ['$scope', 'RecipesService'];
+RecipesController.$inject = ['$scope'];
 
-function RecipesController($scope, RecipesService) {
-    $scope.recipes = RecipesService.query();
+function RecipesController($scope) {
+    
+    'use strict';
+
+    var vm = this;
+
+    vm.notas = [];
+    vm.tipos = ['Llamadas','Personales','Citas','General'];
+    vm.tipo = 'Personales';
+    vm.notaRepetida = false;
+
+    vm.setNewNota = setNewNota;
+
+    function setNewNota(){
+
+        if(vm.newNota && vm.notas.indexOf(vm.newNota) === -1 ){
+            vm.notas.push(vm.newNota);
+            vm.newNota = undefined;
+            $scope.notasForm.$setPristine();
+            vm.notaRepetida = false;
+        } else if(vm.notas.indexOf(vm.newNota) !== -1){
+            vm.notaRepetida = true;
+        }
+
+    }
+
 }
