2936fd3122924f3ac7c38954a4823186f9b2ec0b
Cesar Landeros Delgado <chichozell@gmail.com>
2015-11-09 16:31:41 -0600

    first session working code

--- app/modules/recipes/recipes.config.js
+++ app/modules/recipes/recipes.config.js
@@ -6,6 +6,7 @@ angular.module('Reciper.recipes', ['ngRoute', 'ngResource'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/recipes', {
     templateUrl: 'modules/recipes/recipes_list.html',
-    controller: 'RecipesController'
+    controller: 'RecipesController',
+    controllerAs: 'vm'
   });
 }])
--- app/modules/recipes/recipes.controller.js
+++ app/modules/recipes/recipes.controller.js
@@ -4,64 +4,7 @@
 angular.module('Reciper.recipes')
     .controller('RecipesController', RecipesController);
 
-RecipesController.$inject = ['$scope', 'RecipesService'];
+RecipesController.$inject = ['RecipesService'];
 
-function RecipesController($scope, RecipesService) {
-    $scope.recipes    = RecipesService.query();
-    $scope.categories = [
-        {
-            key: "0",
-            value: "Desayuno"
-        },
-        {
-            key: "1",
-            value: "Comida"
-        },
-        {
-            key: "2",
-            value: "Cena"
-        },
-        {
-            key: "3",
-            value: "Otra"
-        }
-    ];
-
-    $scope.create        = create;
-    $scope.addIngredient = addIngredient;
-    $scope.addStep       = addStep;
-
-    resetFormValues();
-
-    function resetFormValues() {
-        $scope.new_recipe             = {};
-        $scope.new_recipe.ingredients = [{}];
-        $scope.new_recipe.steps       = [{}];
-        $scope.new_recipe.comments    = [];
-    }
-
-    function create(recipe) {
-        console.debug(recipe);
-
-        if (angular.isUndefined(
-                recipe.ingredients[recipe.ingredients.length-1].name)) {
-            recipe.ingredients.pop();
-        }
-
-        if (angular.isUndefined(
-                recipe.steps[recipe.steps.length-1].description)) {
-            recipe.steps.pop();
-        }
-
-        $scope.recipes.unshift(recipe);
-        resetFormValues();
-    }
-
-    function addIngredient() {
-        $scope.new_recipe.ingredients.push({});
-    }
-
-    function addStep() {
-        $scope.new_recipe.steps.push({});
-    }
+function RecipesController(RecipesService) {
 }
