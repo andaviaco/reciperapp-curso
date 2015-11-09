db22b9b465ec6dc6d8994b7e92ab05a2025196bb
Cesar Landeros Delgado <chichozell@gmail.com>
2015-11-09 17:39:15 -0600

    end of class 1

--- app/modules/recipes/recipes.controller.js
+++ app/modules/recipes/recipes.controller.js
@@ -1,10 +1,15 @@
 
-'use strict';
-
 angular.module('Reciper.recipes')
     .controller('RecipesController', RecipesController);
 
 RecipesController.$inject = ['RecipesService'];
 
 function RecipesController(RecipesService) {
+    var vm = this;
+
+    vm.resultado;
+
+    vm.enviarFormulario = function (valor) {
+        vm.resultado = valor;
+    };
 }
