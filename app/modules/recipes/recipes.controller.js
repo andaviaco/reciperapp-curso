ed0ec60465304802ae22166e19cf2b08013feb8f
Cesar Landeros Delgado <chichozell@gmail.com>
2015-11-10 16:41:09 -0600

    new recipe form example

--- app/modules/recipes/recipes.controller.js
+++ app/modules/recipes/recipes.controller.js
@@ -7,9 +7,39 @@ RecipesController.$inject = ['RecipesService'];
 function RecipesController(RecipesService) {
     var vm = this;
 
-    vm.resultado;
-
-    vm.enviarFormulario = function (valor) {
-        vm.resultado = valor;
+    vm.nuevaReceta = {
+      ingredientes: []
     };
+    vm.nuevoIngrediente = {};
+    vm.recetas = [];
+
+    vm.agregarReceta = function (receta) {
+        var ingredienteAgregado = vm.agregarIngrediente(vm.nuevoIngrediente);
+        if (receta.nombre) {
+
+            vm.recetas.push(receta);
+
+            vm.nuevaReceta = {
+                ingredientes: []
+            };
+        } else {
+            alertarError('la receta debe tener un nombre');
+        }
+    }
+    vm.agregarIngrediente = function (ingrediente) {
+        if (ingrediente.nombre) {
+            vm.nuevaReceta.ingredientes.push(ingrediente);
+
+            vm.nuevoIngrediente = {};
+        } else {
+            alertarError('el ingrediente debe tener un nombre');
+
+            return false;
+        }
+
+
+    }
+    function alertarError(error) {
+        alert(error);
+    }
 }
